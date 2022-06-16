17088c7ec48f1b0af9df577042a98ff00576e07e
QuentiumYT <quentin.lienhardt@gmail.com>
2022-06-16 15:44:18 +0200

    Switch to jsonpath-plus

--- extension/src/json-viewer/viewer/apply-jsonpath.js
+++ extension/src/json-viewer/viewer/apply-jsonpath.js
@@ -1,27 +1,27 @@
 var jsonFormatter = require('../jsl-format');
-var jp = require('jsonpath');
+var JSONPath = require('jsonpath-plus/dist/index-browser-umd.cjs');
 
 function applyJsonPath(path, originalValue, highlighter, excludeHeader) {
-    try {
-        var text = originalValue;
-        var header = "";
-        if (excludeHeader) {
-            var lines = text.split("\n");
-            header = lines.splice(0, 3).join("\n");
+  try {
+    var text = originalValue;
+    var header = "";
+    if (excludeHeader) {
+      var lines = text.split("\n");
+      header = lines.splice(0, 3).join("\n");
 
-            text = lines.join("\n");
-        }
-        
-        var json = jp.query(JSON.parse(text), path);
-        var result = "";
+      text = lines.join("\n");
+    }
 
-        if (excludeHeader) {
-            result = header + "\n";
-        }
-        result += jsonFormatter(JSON.stringify(json));
+    var json = JSONPath.JSONPath({ path: path, json: JSON.parse(text) });
+    var result = "";
 
-        highlighter.editor.setValue(result);
-    } catch (e) {}
+    if (excludeHeader) {
+      result = header + "\n";
+    }
+    result += jsonFormatter(JSON.stringify(json));
+
+    highlighter.editor.setValue(result);
+  } catch (e) { }
 }
 
 module.exports = applyJsonPath;
